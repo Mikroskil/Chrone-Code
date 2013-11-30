5402564ae316b24e8b756bbb43ff024678656384
Erwin <rw7nz.cyruz@gmail.com>
2013-11-30 11:47:56 +0700

    add dummy page for testing.

--- index.php
+++ index.php
@@ -1,8 +1,53 @@
-<?php
-$dbconn = pg_connect("host=ec2-23-21-94-137.compute-1.amazonaws.com port=5432 dbname=doeul6s0b6ek user=jdjkyvhhicfzea password=Hf6wiJcig0DBCIXF7yG_HPQ2zf sslmode=require options='--client_encoding=UTF8'") or die('Could not connect: ' . pg_last_error());
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Qattend">
+    <meta name="author" content="Qattend">
+    <link rel="shortcut icon" href="favicon.png">
 
-# $result = pg_query($dbconn, "SELECT statement goes here");
-?>
+    <title>Qattend</title>
 
-<h1>Qattend</h1>
-<p>Connection established.</p>
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap-theme.min.css">
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+    <link href="custom.css" rel="stylesheet">
+
+    <script type="text/javascript">
+      WebFontConfig = {
+        google: { families: [ 'Roboto:400,300,100,700:latin' ] }
+      };
+      (function() {
+        var wf = document.createElement('script');
+        wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+          '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+        wf.type = 'text/javascript';
+        wf.async = 'true';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(wf, s);
+      })();
+    </script>
+  </head>
+  <body>
+
+    <?php include('includes/header.inc'); ?>
+
+    <div class="container marketing">
+
+      <?php include('includes/main.inc'); ?>
+
+      <?php include('includes/footer.inc'); ?>
+
+    </div>
+
+    <script src="//code.jquery.com/jquery-1.10.2.min.js"></script>
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/holder/2.0/holder.js"></script>
+  </body>
+</html>
